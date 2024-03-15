cd496e3eb2ff0714fcca03fb423e4010af15dde3
dipalikh <158269159+dipalikh@users.noreply.github.com>
2024-03-15 16:42:55 +0100

    Update Program.cs
    add .

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//Task 1: Name Input Program
+//Task 1: Name Input Program.
 
 Console.Write("Please Enter your name:");  //Ask the user to enter their name
 
